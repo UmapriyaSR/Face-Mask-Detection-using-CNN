dcff393b4baac63ab711145cf9a81006ba1fcea2
UmapriyaSR <upriya028@outlook.com>
2023-09-26 00:57:59 -0700

    first commit

---
+++ utils.py
@@ -0,0 +1,54 @@
+import numpy as np
+import time
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+
+from torch.autograd import Variable
+
+data_transform = transforms.Compose([transforms.Resize((32,32)),
+                                    transforms.RandomHorizontalFlip(),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+
+label2id = {
+    0: 'No Mask',
+    1: 'Mask'
+}
+
+class CNN(nn.Module):
+    def __init__(self):        _, predicted = torch.max(outputs, 1)
+            correct += (predicted == labels).sum()
+            iterations += 1
+        super(CNN, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=5, stride=1, padding=1),
+            nn.ReLU(),
+            
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=5, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),  #16
+            
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=1, padding=1),
+            nn.ReLU(),
+            
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)   #8
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(in_features=1600, out_features=1024),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+
+            nn.Linear(in_features=1024, out_features=2)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.view(x.shape[0], -1)
+        out = self.fc(x)
+
+        return out
